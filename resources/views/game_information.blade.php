8e354176b45fd0e5666d0340fa0879ab70392923
takuya12345 <10l.mah.s11@gmail.com>
2023-05-04 13:38:29 +0900

    試合情報の一覧表示

--- resources/views/game_information.blade.php
+++ resources/views/game_information.blade.php
@@ -2,16 +2,26 @@
 @section('title', 'スケジュール')
 
 @section('content')
-    <div class="grid grid-cols-4 border-2 border-white text-center m-4">
-        <div class="flex justify-center items-center border-2 border-white p-4">日時</div>
-        <div class="flex justify-center items-center border-2 border-white p-4">場所</div>
-        <div class="flex justify-center items-center border-2 border-white p-4">内容</div>
-        <div class="flex justify-center items-center border-2 border-white p-4">主催</div>
+    <div class="grid grid-cols-4 border-2 border-red-600 text-center m-4">
+        <div class="flex justify-center items-center border-2 border-red-600 p-4">日時</div>
+        <div class="flex justify-center items-center border-2 border-red-600 p-4">場所</div>
+        <div class="flex justify-center items-center border-2 border-red-600 p-4">内容</div>
+        <div class="flex justify-center items-center border-2 border-red-600 p-4">主催</div>
         @foreach ($game_information as $information)
-            <div class="flex justify-center items-center border-2 border-white p-4">{!! nl2br(e($information['date'])) !!}</div>
-            <div class="flex justify-center items-center border-2 border-white p-4">{!! nl2br(e($information['place'])) !!}</div>
-            <div class="flex justify-center items-center border-2 border-white p-4">{!! nl2br(e($information['game_information'])) !!}</div>
-            <div class="flex justify-center items-center border-2 border-white p-4">{!! nl2br(e($information['organizer'])) !!}</div>
+            <div class="flex justify-center items-center border-2 border-red-600 p-4">
+                {!! nl2br(e($information['date'])) !!}
+            </div>
+            <div class="flex justify-center items-center border-2 border-red-600 p-4">
+                <a href="{{ route('attendance') }}" class="underline">
+                    {!! nl2br(e($information['place'])) !!}
+                </a>
+            </div>
+            <div class="flex justify-center items-center border-2 border-red-600 p-4">
+                {!! nl2br(e($information['game_information'])) !!}
+            </div>
+            <div class="flex justify-center items-center border-2 border-red-600 p-4">
+                {!! nl2br(e($information['organizer'])) !!}
+            </div>
         @endforeach
     </div>
 @endsection
